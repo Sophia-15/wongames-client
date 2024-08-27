cc493bb73dab7d935c3b824b2d4d4b435e0bbca4
Sophia <deletrasopa@gmail.com>
2024-08-27 00:53:32 -0300

    Initial commit from Create Next App

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // lint & prettify ts and js files
+  "*.{js, jsx, ts, tsx}": (filenames) => [
+    `prettier --write ${filenames.join(" ")}`,
+    `npm run lint --fix . ${filenames.join(" --file")}`,
+    `npm test -- --findRelatedTests ${filenames.join(" ")}`,
+  ],
+};
